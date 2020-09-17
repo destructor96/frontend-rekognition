73160b6fd5a97dd150897ee5d72af1126f28e46a
destructor96 <mfaheemsharif96@gmail.com>
2020-09-17 19:25:58 +0500

    Complete Frontend for Choosing a file and getting response from Rekognition

---
+++ src/component/file-upload/FileUpload.jsx
@@ -0,0 +1,229 @@
+import React, { Component } from "react";
+import axios from "axios";
+import * as styles from "./FileUpload.module.css";
+
+export default class FileUpload extends Component {
+  state = {
+    fileToUpload: undefined,
+    uploadSuccess: undefined,
+    error: undefined,
+    feedback: false,
+    response: undefined,
+    fileKey: undefined,
+    rekognitionResult: undefined,
+    filePath: undefined,
+    predValueDisabled: true,
+    predValue: undefined,
+    comments: "",
+  };
+
+  uploadFile() {
+    // Getting the signed url
+    axios({
+      url:
+        "https://fcw8650ush.execute-api.us-east-2.amazonaws.com/dev/upload-to-s3?fileName=" +
+        this.state.fileToUpload.name,
+      method: "POST",
+    }).then((response) => {
+      // Getting the url from response
+      const url = response.data.fileUploadURL;
+      console.log(response);
+      this.setState({
+        fileKey: response.data.fileKey,
+        filePath: response.data.filePath,
+      });
+
+      axios({
+        method: "PUT",
+        url: url,
+        data: this.state.fileToUpload,
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+        .then((res) => {
+          this.setState({
+            uploadSuccess: "File upload successfull",
+            error: undefined,
+          });
+
+          axios({
+            method: "PUT",
+            url:
+              "https://fcw8650ush.execute-api.us-east-2.amazonaws.com/dev/get-custom-label",
+            data: { fileName: this.state.fileKey },
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
+          })
+            .then((response) => {
+              this.setState(
+                {
+                  rekognitionResult: response.data,
+                },
+                () => {
+                  this.setState({
+                    feedback: true,
+                  });
+                  console.log(this.state.rekognitionResult.body[0]);
+                }
+              );
+            })
+            .catch((err) => {
+              this.setState({ feedback: false, rekognitionResult: null });
+              console.log(err);
+            });
+        })
+        .catch((err) => {
+          this.setState({
+            error: "Error Occured while uploading the file",
+            uploadSuccess: undefined,
+          });
+        });
+    });
+  }
+
+  updateFeedback = () => {
+    axios({
+      method: "PUT",
+      url:
+        "https://fcw8650ush.execute-api.us-east-2.amazonaws.com/dev/publish-feedback",
+      data: {
+        fileName: this.state.filePath,
+        correctPred: this.state.predValue,
+        comments: this.state.comments,
+      },
+    })
+      .then((response) => {
+        this.setState({
+          rekognitionResult: undefined,
+          feedback: false,
+          fileToUpload: undefined,
+          fileKey: null,
+          filePath: null,
+          predValueDisabled: true,
+        });
+        alert("Feedback Published Successfully");
+        window.location.reload();
+      })
+      .catch((err) => {
+        this.setState({
+          feedback: false,
+          rekognitionResult: null,
+          fileToUpload: undefined,
+          fileKey: null,
+          filePath: null,
+          predValueDisabled: true,
+        });
+        console.log(err);
+        alert("Error Publishing Feedback");
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <div className='logo'>
+          <img src='/cllogo.png' alt='Amazon Rekognition Custom Labels Logo' />
+        </div>
+        <h2>Amazon Rekognition Custom Labels Demo</h2>
+        <div className='powered'>powered by Amazon Rekognition</div>
+        <div className={styles.fileUploadCont}>
+          <div className={styles.header}>
+            File Upload to S3 with Lambda, And React axios Application
+          </div>
+          <div>
+            <form>
+              <div className='form-group'>
+                <input
+                  type='file'
+                  className='form-control-file'
+                  id='fileUpload'
+                  onChange={(e) => {
+                    this.setState({
+                      fileToUpload: e.target.files[0],
+                    });
+                  }}
+                />
+                {this.state.fileToUpload ? (
+                  <button
+                    type='button'
+                    className='btn btn-light'
+                    onClick={(e) => {
+                      this.uploadFile();
+                    }}
+                  >
+                    Upload your file
+                  </button>
+                ) : null}
+
+                <div>
+                  <span>
+                    {this.state.uploadSuccess ? "File Upload Successfully" : ""}
+                  </span>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+        {this.state.feedback ? (
+          <div>
+            <h1>Result from Amazon Rekognition</h1>
+            <h3>Label: {this.state.rekognitionResult.body[0].Name}</h3>
+            <h3>
+              Confidence: {this.state.rekognitionResult.body[0].Confidence}
+            </h3>
+            <div className='form-group'></div>
+            <div className='form-group'>
+              <label htmlFor='simpleInput'>Prediction Correctness</label>{" "}
+              <select
+                name='predValue'
+                id='predValue'
+                onChange={(e) => {
+                  if (e.target.value === "false")
+                    this.setState({ predValueDisabled: true });
+                  else
+                    this.setState({
+                      predValueDisabled: false,
+                      predValue: e.target.value,
+                    });
+                }}
+              >
+                <option value={false}></option>
+                <option value='Yes'>Yes</option>
+                <option value='No'>No</option>
+              </select>
+              <label htmlFor='simpleInput'>Comments</label>{" "}
+              <input
+                type='text'
+                className='feedback-form'
+                id='simpleInput'
+                onChange={(e) => {
+                  this.setState(
+                    {
+                      comments: e.target.value,
+                    },
+                    () => {
+                      console.log(this.state);
+                    }
+                  );
+                }}
+              />
+            </div>
+            <div className='form-group'>
+              <button
+                type='button'
+                className='btn btn-primary'
+                disabled={this.state.predValueDisabled}
+                onClick={this.updateFeedback}
+              >
+                Submit
+              </button>{" "}
+              {/* Some form attributes use an expression to set true or false: they include disabled, required, checked and readOnly */}
+            </div>
+          </div>
+        ) : null}
+      </div>
+    );
+  }
+}
